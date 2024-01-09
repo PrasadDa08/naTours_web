35d2fd54ea6b54e666715ca8afe86a72d0d9a1ce
PrasadDa08 <prasaddatir05@gmail.com>
2024-01-09 15:28:18 +0530

    Final Changes

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ export const login = async (email, password) => {
   try {
     const result = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: 'https://natours-web-e50y.onrender.com/login',
       data: {
         email,
         password,
